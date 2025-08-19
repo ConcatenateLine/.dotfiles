76a8f9f2ede05800ca96d83dc4cdc17671b35e32
Concatenate Line <123336223+ConcatenateLine@users.noreply.github.com>
2025-08-18 22:59:25 -0600

    feature (smear cursor): add smeat cursor to config lvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -6,6 +6,29 @@
 
 lvim.plugins = {
   {
+    "sphamba/smear-cursor.nvim",
+
+    opts = {
+      -- Smear cursor when switching buffers or windows.
+      smear_between_buffers = true,
+
+      -- Smear cursor when moving within line or to neighbor lines.
+      -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+      smear_between_neighbor_lines = true,
+
+      -- Draw the smear in buffer space instead of screen space when scrolling
+      scroll_buffer_space = true,
+
+      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+      -- Smears will blend better on all backgrounds.
+      legacy_computing_symbols_support = false,
+
+      -- Smear cursor in insert mode.
+      -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+      smear_insert_mode = true,
+    },
+  },
+  {
     'smoka7/hop.nvim',
     event = "BufRead",
     config = function()
@@ -186,7 +209,7 @@ lvim.plugins = {
       require("muren").setup()
     end
   },
- }
+}
 
 lvim.builtin.which_key.mappings["r"] = {
   name = "Replace",
@@ -278,4 +301,3 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
     if vim.fn.argc() == 0 then require("alpha").start() end
   end,
 })
-
